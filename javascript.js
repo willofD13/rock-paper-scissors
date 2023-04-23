436fba0656dbf6298e525d50972dd046ef636d98
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-04-23 22:16:57 +0300

    Initialize computerSelection after Items

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 let playerSelection = prompt('Enter your choice', '');
 console.log(playerSelection);
-let computerSelection = getComputerChoice();
 
 let items = ['Rock', 'Paper', 'Scissors'];
 
+let computerSelection = getComputerChoice();
 
 function getComputerChoice() {
   return items[Math.floor(Math.random()*items.length)]
