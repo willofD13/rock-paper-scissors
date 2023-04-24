fb6872d6112052e8aa5213f836e601cf8d3be34f
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-04-24 21:04:12 +0300

    Initialize a variable with the result of the game

--- javascript.js
+++ javascript.js
@@ -1,41 +1,49 @@
-function game(){
-function getPlayerChoice() {
-  return prompt('Enter your Choice' ,'');
-}
-let playerSelection = getPlayerChoice();
-console.log(playerSelection);
-
-let items = ['Rock', 'Paper', 'Scissors'];
-
-
-
-function getComputerChoice() {
-  return items[Math.floor(Math.random()*items.length)]
-}
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-
- function playRound(playerSelection, computerSelection){
-  if ((playerSelection.toLowerCase() === 'rock') && (computerSelection=== 'Rock') 
-  || (playerSelection.toLowerCase()=== 'paper') && (computerSelection === 'Paper')
-  || (playerSelection.toLowerCase()=== 'scissors') && (computerSelection==='Scissors')) {
-    return 'it is a tie'
+function game() {
+  function getPlayerChoice() {
+    return prompt('Enter your Choice' ,'');
   }
-  else if ((playerSelection.toLowerCase()=== 'rock') && (computerSelection=== 'Paper')
-  || (playerSelection.toLowerCase()=== 'paper') && (computerSelection=== 'Scissors')
-  || (playerSelection.toLowerCase()=== 'scissors') && (computerSelection=== 'Rock')) {
-    return 'you lose'
+  let playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+  
+  let items = ['Rock', 'Paper', 'Scissors'];
+  
+  
+  
+  function getComputerChoice() {
+    return items[Math.floor(Math.random()*items.length)]
   }
-  else {return 'you win'}
+  let computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  
+   function playRound(playerSelection, computerSelection){
+    if ((playerSelection.toLowerCase() === 'rock') && (computerSelection=== 'Rock') 
+    || (playerSelection.toLowerCase()=== 'paper') && (computerSelection === 'Paper')
+    || (playerSelection.toLowerCase()=== 'scissors') && (computerSelection==='Scissors')) {
+      return 'it is a tie'
+    }
+    else if ((playerSelection.toLowerCase()=== 'rock') && (computerSelection=== 'Paper')
+    || (playerSelection.toLowerCase()=== 'paper') && (computerSelection=== 'Scissors')
+    || (playerSelection.toLowerCase()=== 'scissors') && (computerSelection=== 'Rock')) {
+      return 'you lose'
+    }
+    else {return 'you win'}
+  }
+  
+  
+  let result = playRound(playerSelection,computerSelection);
+  console.log(result);
 }
-console.log(playRound(playerSelection,computerSelection));
-}
-game();
-game();
-game();
-game();
-game();
-
+  
+  
+  
+     
+  
+     
+  
+  
+  
+  
+  
 
    
 
